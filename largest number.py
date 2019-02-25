fe3cc216a5bdc1ce20f45607c606810d85c68e19
tharanisl <46369742+tharanisl@users.noreply.github.com>
2019-02-25 14:09:02 +0530

    Create largest number.py

---
+++ largest number.py
@@ -0,0 +1,13 @@
+num1=100
+num2=140
+num3=120
+num1=("enter the 1st number:");
+num2=("enter the snd number:");
+num3=("enter the 3rd number:");
+if(num1>=num2)and (num1>=num3)
+largest=num1
+elseif(num2>=num1)and (num2>=num3)
+largest=num2
+else 
+largest=num3
+print(" the largest number between","num1","num2","num3","is largest")
